f8be844221ef1bc4e575c606f0c93fe78c3119b0
Pratham Dogra <dogra.pratham@gmail.com>
2019-10-14 21:33:05 +0530

    create-react-app

---
+++ client/src/components/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+const Main =()=>{
+    return (
+      <Router>
+        <Route path="/" exact component={} />
+        <Route path="/" component={} />
+        <Route path="/" component={} />
+        
+        
+      </Router>
+    );
+}
