b4c7b0df1075ab65fd6f2174fe7ce350d7d54528
Pratham Dogra <dogra.pratham@gmail.com>
2019-10-15 23:53:23 +0530

    New commit

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,14 +1,15 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import Join from './Join';
+import Chat from './Chat';
 
-const Main =()=>{
-    return (
-      <Router>
-        <Route path="/" exact component={} />
-        <Route path="/" component={} />
-        <Route path="/" component={} />
-        
-        
-      </Router>
-    );
-}
+const Main = () => {
+  return (
+    <Router>
+      <Route path="/" exact component={Join} />
+      <Route path="/chat" component={Chat} />
+    </Router>
+  );
+};
+
+export default Main;
---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const http = require("http");
+
+const socketio = require("socket.io");
+const PORT = process.env.PORT || 5000;
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+server.listen(PORT, () => console.log(`Server is running at ${PORT}`));
